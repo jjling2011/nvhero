2db3809b094833b8a57de586049accf53d0a6ec8
jjling <jjling2011@gmail.com>
2018-11-17 17:49:15 +0800

    update

--- res/scripts/startV2Ray.sh
+++ res/scripts/startV2Ray.sh
@@ -5,7 +5,7 @@ cd ${root}
 
 if [ ! -f /res/v2ray/v2ray.zip ]; then
   wget -O v2ray.zip \
-      http://github.com/v2ray/v2ray-core/releases/download/v${VER}/v2ray-linux-64.zip
+      https://github.com/v2ray/v2ray-core/releases/download/v${VER}/v2ray-linux-64.zip
 fi
 
 unzip v2ray.zip
@@ -16,5 +16,6 @@ cp ${root}/config_server.json ./config.json
 sed -i "s/__UUID__/${UUID}/g" ./config.json
 sed -i "s#__WSPATH__#${WSPATH}#g" ./config.json
 
+chmod +x v2ctl
 chmod +x v2ray
 ./v2ray
