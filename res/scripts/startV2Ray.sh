4633e36a7f84d456b489c9d317438005167f2b4e
jjling <jjling2011@gmail.com>
2020-02-03 16:32:42 +0800

    use vmess

--- res/scripts/startV2Ray.sh
+++ res/scripts/startV2Ray.sh
@@ -6,15 +6,13 @@ cd ${root}
 if [ "x${REMOTE_SERVER}" == "xon" ]; then
   cp ${root}/config_relay.json ./config.json
   sed -i "s/__OUT_SERVER__/${OUT_SERVER}/g" ./config.json
-  sed -i "s/__OUT_USERNAME__/${OUT_USERNAME}/g" ./config.json
-  sed -i "s/__OUT_PASSWORD__/${OUT_PASSWORD}/g" ./config.json
+  sed -i "s/__OUT_UUID__/${OUT_UUID}/g" ./config.json
   sed -i "s#__OUT_WSPATH__#${OUT_WSPATH}#g" ./config.json
 else
   cp ${root}/config_outlet.json ./config.json
 fi
 
-sed -i "s/__IN_USERNAME__/${IN_USERNAME}/g" ./config.json
-sed -i "s/__IN_PASSWORD__/${IN_PASSWORD}/g" ./config.json
+sed -i "s/__IN_UUID__/${IN_UUID}/g" ./config.json
 sed -i "s#__IN_WSPATH__#${IN_WSPATH}#g" ./config.json
 
 chmod +x v2ctl
